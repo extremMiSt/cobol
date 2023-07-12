7e87133100d41e967de46f58e5d101e40044430a
Michael Steinle <michael.steinle@gmail.com>
2023-07-12 12:57:42 +0200

    aoc 22 day 1 task 2

--- aoc2201_2.cbl
+++ aoc2201_2.cbl
@@ -11,7 +11,10 @@
        FD inputfile RECORD CONTAINS 80 CHARACTERS.
        01 inputline PIC X(80).
        WORKING-STORAGE SECTION.
-       01 max PIC 9(10) VALUE 0.
+       01 max1 PIC 9(10) VALUE 0.
+       01 max2 PIC 9(10) VALUE 0.
+       01 max3 PIC 9(10) VALUE 0.
+       01 fin PIC 9(10).
        01 cur PIC 9(10) VALUE 0.
        01 eof PIC 9 VALUE 0.
        PROCEDURE DIVISION.
@@ -20,15 +23,26 @@
            PERFORM UNTIL eof > 0
               READ inputfile AT END MOVE 1 TO eof NOT AT END
               IF inputline NOT > SPACES
-                 IF max < cur THEN
-                    MOVE cur TO max
+                 IF max1 < cur THEN
+                    MOVE max2 TO max3
+                    MOVE max1 TO max2
+                    MOVE cur TO max1
+                    MOVE 0 TO cur 
+                 ELSE IF max2 < cur THEN
+                    MOVE max2 TO max3 
+                    MOVE cur TO max2
+                    MOVE 0 TO cur 
+                 ELSE IF max3 < cur THEN
+                    MOVE cur TO max3
+                    MOVE 0 TO cur
                  END-IF
                  MOVE 0 TO cur
               ELSE 
                  COMPUTE cur = cur + FUNCTION NUMVAL(inputline)
               END-IF
            END-PERFORM.
-           DISPLAY max.
+           COMPUTE fin = max1+max2+max3.
+           DISPLAY fin.
            CLOSE inputfile.
            STOP RUN.
        END PROGRAM AOC2201_1.
